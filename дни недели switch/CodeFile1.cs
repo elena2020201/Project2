3d290cdece364f22289ddf45969cb831f9e0f041
elena2020201 <elenarus1992@mail.ru>
2021-05-24 18:27:08 +0300

    Добавьте файлы проекта.

---
+++ дни недели switch/CodeFile1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+class Switch
+{
+    static void Main()
+    {
+        try
+        {
+            int number;
+            string name;
+            number = Int32.Parse(Interaction.InputBox("Введите число дня в неделе:", "Число"));
+            switch (number)
+            {
+                case 1:
+                    name = "Сегодня понедельник";
+                    break;
+                case 2:
+                    name = "Сегодня вторник";
+                    break;
+                case 3:
+                    name = "Сегодня среда";
+                    break;
+                case 4:
+                    name = "Сегодня четверг";
+                    break;
+                case 5:
+                    name = "Сегодня пятница";
+                    break;
+                case 6:
+                    name = "Сегодня суббота";
+                    break;
+                case 7:
+                    name = "Сегодня воскресенье";
+                    break;
+                default:
+                    name = "Такого дня в неделе нет!";
+                    break;
+            }
+            MessageBox.Show(name, "Число дня в неделе");
+        }
+        catch
+        {
+            MessageBox.Show("Введите число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        MessageBox.Show("Программа завершена", "Завершение");
+    }
+        }
